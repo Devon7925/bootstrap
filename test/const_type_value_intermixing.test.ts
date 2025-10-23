f69c77dba049fa8b3ef379f7ecd58a9dd6b26f27
Devon7925 <devonrutledge03@gmail.com>
2025-10-22 22:17:39 -0600

    Add new todo test

--- test/const_type_value_intermixing.test.ts
+++ test/const_type_value_intermixing.test.ts
@@ -231,3 +231,21 @@ test("const fn with only const parameters can use let for tuple in array types",
     const result = await runWasmMainWithGc(wasm);
     expect(result).toBe(42);
 });
+
+test.todo("const fn with only const parameters can return type array", async () => {
+    const wasm = await compileWithAstCompiler(`
+    const fn foo(const STR_LEN: i32) -> [type; STR_LEN] {
+        let entries: [type; STR_LEN] =
+            [i32; STR_LEN];
+        entries
+    }
+
+    const BAR: [type; 3] = foo(3);
+
+    fn main() -> i32 {
+        42
+    }
+    `);
+    const result = await runWasmMainWithGc(wasm);
+    expect(result).toBe(42);
+});
